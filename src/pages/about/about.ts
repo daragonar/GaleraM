c1262c94707c08b692b384fe8ff194e6378bf81f
daragonar <daragonar@hotmail.com>
2017-04-15 11:32:09 +0200

    Subida inicial de proyecto vacio

---
+++ src/pages/about/about.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController) {
+
+  }
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+}
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { ActionSheetController } from 'ionic-angular';
+
+
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+
+
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = AboutPage;
+  tab3Root = ContactPage;
+
+  constructor(public actionSheetCtrl: ActionSheetController) {
+
+  }
+
+ presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+  
+}
