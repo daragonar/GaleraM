d876b973b5ab867a464fa2cad8bb34c9857c3642
albertoi <alberto.eps@gmail.com>
2017-05-29 13:24:48 +0200

    Añadido slim php a carpeta /api para preparar api propia

---
+++ api/index.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+// Configuración de cabeceras
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
+$app->get("/pruebas", function() use($app){
+    echo "Hola mundo desde Slim PHP";
+});
+
+$app->run();
